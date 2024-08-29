bca1636755350ecb5eb1ea3b1f13e09840b2697d
aiiiiispring <1554454156@qq.com>
2024-08-29 17:17:41 +0800

    增加分页插件

---
+++ myself-core/src/main/java/com/myself/core/interceptor/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.myself.core.interceptor;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author aiiiiii
+ * @CreateTime: 2024/08/29/17:12
+ * MybatisPlus配置 分页插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
